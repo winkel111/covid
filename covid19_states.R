8e06e4323e3741e6360fa4ca6daeefa4f1b4a783
winkel111 <62120893+winkel111@users.noreply.github.com>
2020-06-17 10:17:18 -0400

    Update covid19_states.R
    Speed up calculation by removing redundant code

--- covid19_states.R
+++ covid19_states.R
@@ -61,10 +61,10 @@ popstate <- popul_states %>%
 popo <- as.numeric(popstate[1,2])
 
 #Get case data
-if (!(exists("case"))) {
+if (!(exists("casus"))) {
    #case <- read_csv(url("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"), col_types = cols())
    #case <- read_csv(url("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"), col_types = cols())
-   case <- read_csv(url("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"), col_types = cols())
+   #case <- read_csv(url("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"), col_types = cols())
    casus <- read_csv(url("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"), col_types = cols())
    #casus <- read_csv(url("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv"), col_types = cols())
 
@@ -77,72 +77,113 @@ if (!(exists("case"))) {
      #dplyr::filter(`Province_State` %in% state.name) %>%
      select(12:ncol(casus)) %>%
      colSums()
+   
+   #Create data frame for US cases
+   pdata <- data.frame(date = names(cases), cases = cases)
+   rownames(pdata) <- c()
+   pdata$date <- as.Date(pdata$date, format = "%m/%d/%y")
+   
+   #Model fit
+   Xp <- as.numeric(rownames(pdata))
+   Yp <- pdata[,2]
+   spdata <- cbind.data.frame(Xp,Yp)
+   
+   #Calculate differences
+   dsp <- spdata %>% mutate(Diff = Yp - lag(Yp))
+   dpdata <- data.frame(pdata[,1],dsp[,3])
+   names(dpdata) <- c("date","diff")
+   
+   #Linear model fit over the 4 most recent days
+   fstartp <- nrow(spdata)-3
+   lmodelp <- lm(formula = Yp ~ Xp,  data = na.omit(spdata[fstartp:nrow(spdata),]))
+   qMp <- summary(lmodelp)
+   qkp <-summary(lmodelp)$coefficients[2, 1]
+   print("United States:")
+   print(qMp)
+   
+   #Start for fit
+   qfp <- tail(pdata[,1],1)-3
+   
+   #Latest number of cases
+   latestp <- tail(pdata[,2],1)
+   
+   #Latest number of new cases
+   recentp <- diff(tail(pdata[,2],2))
+   
+   #Percent penetration
+   percp <- round(tail(pdata[,2],1)/popp*100,2)
+   
+   #Plot parameters
+   fsize <- 22
+   psize <- 5
+   lsize <- 2
+   
+   #Plot cumulative cases in US
+   qp1 <- ggplot(pdata, aes(x=date, y=cases))
+   qp1 <- qp1 + theme_bw(base_size = fsize) #+ theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+   qp1 <- qp1 + geom_point(color="blue", size=psize)
+   qp1 <- qp1 + stat_smooth(data=subset(pdata, date >= qfp),method="lm", color="gray40", size=lsize, se = FALSE, level = 0.95)
+   #qp1 <- qp1 + stat_function(fun = function(x) fModel(x, k=coef(lmodelp)["Xp"], d=coef(lmodelp)["(Intercept)"]), size=lsize, color="gray40")
+   #qp1 <- qp1 + geom_line(data = modelfit, aes(date, y=cases), color="firebrick", size=lsize)
+   #qp1 <- qp1 + stat_function(fun = function(x) fModel(x, a=qa, b=qb, c=qc), size=lsize, color="firebrick")
+   qp1 <- qp1 + scale_x_date(date_breaks = "2 weeks", date_labels = "%Y/%m/%d") # + scale_y_log10()
+   qp1 <- qp1 + scale_y_continuous(breaks = scales::pretty_breaks(n = 6),labels = comma)
+   qp1 <- qp1 + theme(axis.text.x = element_text(angle = 30, hjust = 1))
+   qp1 <- qp1 + xlab(expression("Date")) #+ scale_y_log10()
+   qp1 <- qp1 + ylab(expression("US cases"))
+   #qp1 <- qp1 + expand_limits(x=c(pdata[1,1], as.Date("2020-04-08")))
+   qp1 <- qp1 + labs(caption=paste(c(latestp," cases\n",percp,"% of population","\n",qkp," cases per day","\nLast update: ",as.character(tail(pdata[,1],1))," - New cases: ",recentp,"\nData source: Johns Hopkins University Center for Systems Science and Engineering (JHU CSSE)"), collapse = ""))
+   qp1 <- qp1 + theme(plot.caption=element_text(size=fsize/2, hjust=0, margin=margin(12,0,0,0)))
+   
+   #Plot daily cases in US
+   qp3 <- ggplot(dpdata, aes(x=date, y=diff))
+   qp3 <- qp3 + theme_bw(base_size = fsize) #+ theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+   qp3 <- qp3 + geom_point(color="blue", size=psize)
+   qp3 <- qp3 + geom_smooth(color="gray50", method="gam", size=lsize)
+   #qp3 <- qp3 + stat_smooth(data=subset(pdata, date >= qfp),method="lm", color="gray40", size=lsize, se = FALSE, level = 0.95)
+   #qp3 <- qp3 + stat_function(fun = function(x) fModel(x, k=coef(lmodelp)["Xp"], d=coef(lmodelp)["(Intercept)"]), size=lsize, color="gray40")
+   #qp3 <- qp3 + geom_line(data = modelfit, aes(date, y=cases), color="firebrick", size=lsize)
+   #qp3 <- qp3 + stat_function(fun = function(x) fModel(x, a=qa, b=qb, c=qc), size=lsize, color="firebrick")
+   qp3 <- qp3 + scale_x_date(date_breaks = "2 weeks", date_labels = "%Y/%m/%d") # + scale_y_log10()
+   qp3 <- qp3 + scale_y_continuous(breaks = scales::pretty_breaks(n = 6),labels = comma)
+   qp3 <- qp3 + theme(axis.text.x = element_text(angle = 30, hjust = 1))
+   qp3 <- qp3 + xlab(expression("Date")) #+ scale_y_log10()
+   qp3 <- qp3 + ylab(expression("US daily cases"))
+   #qp3 <- qp3 + expand_limits(x=c(pdata[1,1], as.Date("2020-04-08")))
+   #qp3 <- qp3 + labs(caption=paste(c(latestp," cases\n",percp,"% of population","\n",qkp," cases per day","\nLast update: ",as.character(tail(pdata[,1],1)),"\nData source: Johns Hopkins University Center for Systems Science and Engineering (JHU CSSE)"), collapse = ""))
+   #qp3 <- qp3 + theme(plot.caption=element_text(size=8, hjust=0, margin=margin(12,0,0,0)))
+   
 }
 
-if (state != "") {
-  cases_other <- casus %>%
-    mutate_all(~replace(., is.na(.), 0)) %>%
-    dplyr::filter(`Country_Region` == country) %>%
-    # dplyr::filter(`Province_State` == state) %>%
-    dplyr::filter(str_detect(`Province_State`, state)) %>%
-    select(12:ncol(casus)) %>%
-    colSums()
-    country <- state
-} else {
-
-#Select data for other countries
-cases_other <- case %>%
+cases_other <- casus %>%
   mutate_all(~replace(., is.na(.), 0)) %>%
-  dplyr::filter(`Country/Region` == country) %>%
-  select(5:ncol(case)) %>%
+  dplyr::filter(`Country_Region` == country) %>%
+  # dplyr::filter(`Province_State` == state) %>%
+  dplyr::filter(str_detect(`Province_State`, state)) %>%
+  select(12:ncol(casus)) %>%
   colSums()
-}
+country <- state
 
-#plot(cases)
-
-#Create data frame for US cases
-pdata <- data.frame(date = names(cases), cases = cases)
-rownames(pdata) <- c()
-pdata$date <- as.Date(pdata$date, format = "%m/%d/%y")
-#X1 <- as.Date(pdata[,1],format = "%m/%d/%y")
-
-#Create data frame for cases from others
+#Create data frame for cases from states
 odata <- data.frame(date = names(cases_other), cases = cases_other)
 rownames(odata) <- c()
 odata$date <- as.Date(odata$date, format = "%m/%d/%y")
 
-#Model fit
-Xp <- as.numeric(rownames(pdata))
-Yp <- pdata[,2]
-spdata <- cbind.data.frame(Xp,Yp)
-
+#Model fit for states
 Xo <- as.numeric(rownames(odata))
 Yo <- odata[,2]
 sodata <- cbind.data.frame(Xo,Yo)
 
 #Calculate differences
-dsp <- spdata %>% mutate(Diff = Yp - lag(Yp))
 dso <- sodata %>% mutate(Diff = Yo - lag(Yo))
-dpdata <- data.frame(pdata[,1],dsp[,3])
 dodata <- data.frame(odata[,1],dso[,3])
-names(dpdata) <- c("date","diff")
 names(dodata) <- c("date","diff")
 
 #Linear model fit over the 4 most recent days
-fstartp <- nrow(spdata)-3
 fstarto <- nrow(sodata)-3
-
-lmodelp <- lm(formula = Yp ~ Xp,  data = na.omit(spdata[fstartp:nrow(spdata),]))
 lmodelo <- lm(formula = Yo ~ Xo,  data = na.omit(sodata[fstarto:nrow(sodata),]))
-
-qMp <- summary(lmodelp)
 qMo <- summary(lmodelo)
-
-qkp <-summary(lmodelp)$coefficients[2, 1]
 qko <- summary(lmodelo)$coefficients[2, 1]
-
-print("United States:")
-print(qMp)
 print(paste(c(country,":"), collapse = ""))
 print(qMo)
 
@@ -151,25 +192,16 @@ fModel <- function(x, k, d) {
   (k*x+d)
 }
 
-#Plot the data
-fsize <- 26
-psize <- 5
-lsize <- 2
-
 #Start for fit
-qfp <- tail(pdata[,1],1)-3
 qfo <- tail(odata[,1],1)-3
 
 #Latest number of cases
-latestp <- tail(pdata[,2],1)
 latesto <- tail(odata[,2],1)
 
 #Latest number of new cases
-recentp <- diff(tail(pdata[,2],2))
 recento <- diff(tail(odata[,2],2))
 
 #Percent penetration
-percp <- round(tail(pdata[,2],1)/popp*100,2)
 perco <- round(tail(odata[,2],1)/popo*100,2)
 perc_states <- c(perc_states, perco)
 
@@ -178,22 +210,7 @@ perc_states <- c(perc_states, perco)
 #  c( sapply( major_labs, function(x) c(x, rep("", 4) ) ) )
 #labs[1:(length(labs)-n_minor)]}
 
-qp1 <- ggplot(pdata, aes(x=date, y=cases))
-qp1 <- qp1 + theme_bw(base_size = fsize) #+ theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
-qp1 <- qp1 + geom_point(color="blue", size=psize)
-qp1 <- qp1 + stat_smooth(data=subset(pdata, date >= qfp),method="lm", color="gray40", size=lsize, se = FALSE, level = 0.95)
-#qp1 <- qp1 + stat_function(fun = function(x) fModel(x, k=coef(lmodelp)["Xp"], d=coef(lmodelp)["(Intercept)"]), size=lsize, color="gray40")
-#qp1 <- qp1 + geom_line(data = modelfit, aes(date, y=cases), color="firebrick", size=lsize)
-#qp1 <- qp1 + stat_function(fun = function(x) fModel(x, a=qa, b=qb, c=qc), size=lsize, color="firebrick")
-qp1 <- qp1 + scale_x_date(date_breaks = "2 weeks", date_labels = "%Y/%m/%d") # + scale_y_log10()
-qp1 <- qp1 + scale_y_continuous(breaks = scales::pretty_breaks(n = 6),labels = comma)
-qp1 <- qp1 + theme(axis.text.x = element_text(angle = 30, hjust = 1))
-qp1 <- qp1 + xlab(expression("Date")) #+ scale_y_log10()
-qp1 <- qp1 + ylab(expression("US cases"))
-#qp1 <- qp1 + expand_limits(x=c(pdata[1,1], as.Date("2020-04-08")))
-qp1 <- qp1 + labs(caption=paste(c(latestp," cases\n",percp,"% of population","\n",qkp," cases per day","\nLast update: ",as.character(tail(pdata[,1],1))," - New cases: ",recentp,"\nData source: Johns Hopkins University Center for Systems Science and Engineering (JHU CSSE)"), collapse = ""))
-qp1 <- qp1 + theme(plot.caption=element_text(size=fsize/2, hjust=0, margin=margin(12,0,0,0)))
-
+#Plot cumulative cases in states
 qp2 <- ggplot(odata, aes(x=date, y=cases))
 qp2 <- qp2 + theme_bw(base_size = fsize) #+ theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 qp2 <- qp2 + geom_point(color="red", size=psize)
@@ -207,23 +224,7 @@ qp2 <- qp2 + ylab(paste("Cases in",country))
 qp2 <- qp2 + labs(caption=paste(c(latesto," cases\n",perco,"% of population","\n",qko," cases per day","\nLast update: ",as.character(tail(odata[,1],1))," - New cases: ",recento,"\nData source: Johns Hopkins University Center for Systems Science and Engineering (JHU CSSE)"),collapse = ""))
 qp2 <- qp2 + theme(plot.caption=element_text(size=fsize/2, hjust=0, margin=margin(12,0,0,0)))
 
-qp3 <- ggplot(dpdata, aes(x=date, y=diff))
-qp3 <- qp3 + theme_bw(base_size = fsize) #+ theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
-qp3 <- qp3 + geom_point(color="blue", size=psize)
-qp3 <- qp3 + geom_smooth(color="gray50", method="gam", size=lsize)
-#qp3 <- qp3 + stat_smooth(data=subset(pdata, date >= qfp),method="lm", color="gray40", size=lsize, se = FALSE, level = 0.95)
-#qp3 <- qp3 + stat_function(fun = function(x) fModel(x, k=coef(lmodelp)["Xp"], d=coef(lmodelp)["(Intercept)"]), size=lsize, color="gray40")
-#qp3 <- qp3 + geom_line(data = modelfit, aes(date, y=cases), color="firebrick", size=lsize)
-#qp3 <- qp3 + stat_function(fun = function(x) fModel(x, a=qa, b=qb, c=qc), size=lsize, color="firebrick")
-qp3 <- qp3 + scale_x_date(date_breaks = "2 weeks", date_labels = "%Y/%m/%d") # + scale_y_log10()
-qp3 <- qp3 + scale_y_continuous(breaks = scales::pretty_breaks(n = 6),labels = comma)
-qp3 <- qp3 + theme(axis.text.x = element_text(angle = 30, hjust = 1))
-qp3 <- qp3 + xlab(expression("Date")) #+ scale_y_log10()
-qp3 <- qp3 + ylab(expression("US daily cases"))
-#qp3 <- qp3 + expand_limits(x=c(pdata[1,1], as.Date("2020-04-08")))
-#qp3 <- qp3 + labs(caption=paste(c(latestp," cases\n",percp,"% of population","\n",qkp," cases per day","\nLast update: ",as.character(tail(pdata[,1],1)),"\nData source: Johns Hopkins University Center for Systems Science and Engineering (JHU CSSE)"), collapse = ""))
-#qp3 <- qp3 + theme(plot.caption=element_text(size=8, hjust=0, margin=margin(12,0,0,0)))
-
+#Plot daily cases in states
 qp4 <- ggplot(dodata, aes(x=date, y=diff))
 qp4 <- qp4 + theme_bw(base_size = fsize) #+ theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 qp4 <- qp4 + geom_point(color="red", size=psize)
@@ -241,7 +242,7 @@ qp4 <- qp4 + ylab(paste("Daily cases in",country))
 #qp4 <- qp4 + labs(caption=paste(c(latestp," cases\n",percp,"% of population","\n",qkp," cases per day","\nLast update: ",as.character(tail(pdata[,1],1)),"\nData source: Johns Hopkins University Center for Systems Science and Engineering (JHU CSSE)"), collapse = ""))
 #qp4 <- qp4 + theme(plot.caption=element_text(size=8, hjust=0, margin=margin(12,0,0,0)))
 
-
+#Generate figure
 gridimg <- arrangeGrob(qp1, qp2, qp3, qp4, ncol=2)
 grid.draw(gridimg)
 gridus <- arrangeGrob(qp1, qp3, ncol=1)
@@ -253,7 +254,7 @@ ggsave(gridimg, file=paste(c(path,"us_states_",country,".png"), collapse = ""), 
 
 }
 
-#Plot penetration
+#Collect data on exposed population
 df <- data.frame(state.name,perc_states)
 pen <- df[order(-perc_states),]
 pen$state.name <- factor(pen$state.name, levels = pen$state.name)
